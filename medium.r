9214a6ad7d4145bd116ce07d20d66ae53695838f
jasonNikolaou <nikolaouiasonas13@gmail.com>
2020-03-16 02:40:13 +0200

    simplify RCP function

--- medium.r
+++ medium.r
@@ -8,11 +8,16 @@ dot <- function(a, b) {
 }
 
 #distance
-dist <- function(x1, x2) {
+dist = function(x1, x2) {
   return(sqrt(sum((x1 - x2) ^ 2)))
 }
 
-#find min point
+#calculate number of samples
+numOfSamples = function(k, epsilon) {
+  return (ceiling(2.2 * log(1/epsilon) + 1.1 + 0.0505*log(k)))
+}
+
+# find min point
 find_min = function(points, c) {
   min_dot = dot(c, points[,1])
   min = points[,1]
@@ -27,27 +32,28 @@ find_min = function(points, c) {
 }
 
 #randomized cutting plane algorithm
-# P: Polytope in H-representation
-# walk: WalkType for sampling
-# N: number of samples
+# A, b: Polytope in H-representation, i.e. Ax <= b
 # c: cost vector
-# err: calculate an err-optimal solution
-
-RCP = function(A, b, walk, N, c, err) {
+# walk: WalkType for sampling
+# epsilon: calculate an err-optimal solution
+# precision: required precision of the answer
+RCP = function(A, b, c, walk="CDHR", epsilon=0.001, precision=0.0001) {
   P = Hpolytope$new(A, b)
-  points = sample_points(P, WalkType=walk, N=d)
-  A = rbind(A, c)
-  b = c(b, 0)
-  NROWS = length(b)
+  k = 1;
+  N_k = numOfSamples(k, epsilon)
+  points = sample_points(P, WalkType=walk, N=N_k)
+  P$A = rbind(A, c);
   x = 0
   x_new = 0
   repeat {
     x = x_new
     x_new = find_min(points, c)
-    b[NROWS] = dot(c, x_new)
-    P = Hpolytope$new(A, b)
-    points = sample_points(P, WalkType=walk, N=d)
-    if (dist(x, x_new) < err) {
+    P$b = c(b, dot(c, x_new)) #update polytope
+    k = k + 1
+    N_k = numOfSamples(k, epsilon)
+    points = sample_points(P, WalkType=walk, N=N_k)
+    
+    if (dist(x, x_new) < precision) {
       return(x_new)
       break;
     }
@@ -55,28 +61,26 @@ RCP = function(A, b, walk, N, c, err) {
 }
 #=== example 1 =====
 #expected output = (0, 0, 0)
-
-#dimensions:
-d = 3
 #cost:
 c = c(1,1,1)
 # Ax <= b
 A = rbind(c(1,0,0), c(0,1,0), c(0,0,1),
            c(-1,0,0), c(0,-1,0), c(0,0,-1))
 b = c(1,1,1,0,0,0)
-#error
-err = 0.000000001
-#unit cube
+#parameters
+epsilon = 0.0001
+precision = 0.000000001
 walk = "RDHR"
-RCP(A, b, walk, d, c, err)
+
+RCP(A, b, c, walk, epsilon, precision)
 
 #=== example 2 ====
 #expected output = (0, 200)
-d = 2
-err = 0.000000001
+epsilon = 0.00001
+precision = 0.0000001
 c = c(-1, 1)
 A = rbind(c(1,0), c(0,1), c(1,1),
           c(-1,0), c(0,-1))
 b = c(200, 300, 400, 0, 0)
 walk = "BW"
-RCP(A, b, walk, d, c, err)
+RCP(A, b, c, walk, epsilon, precision)
